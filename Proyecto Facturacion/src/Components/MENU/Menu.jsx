dc67c8351dcdbdfd6993cc7d9212097cc96083da
Josue <Josuepinro21@unitec.edu>
2023-02-03 07:29:33 -0600

    Base_PantallaMenu

---
+++ Proyecto Facturacion/src/Components/MENU/Menu.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
+
+function LOGIN() {
+  return (
+    <div>
+      <Container>
+        <Row className="vh-100 d-flex justify-content-center align-items-center">
+          <Col md={88} lg={100} xs={15}>
+            <div className="border border-3 border-primary"></div>
+            <Card className="shadow">
+              <Card.Body>
+                <div className="mb-3 mt-md-4">
+                  <h2 className="fw-bold mb-2 text-uppercase ">
+                    Iniciar Sesion
+                  </h2>
+                  <p>Por favor ingrese correo electrónico y contraseña</p>
+                  <div className="mb-3">
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="text-center">
+                          Direccion de correo
+                        </Form.Label>
+                        <Form.Control
+                          type="email"
+                          placeholder="Ingresar correo"
+                          onChange={(e) => setField("email", e.target.value)}
+                        />
+                      </Form.Group>
+
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicPassword"
+                      >
+                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Control type="password" placeholder="Contraseña" />
+                      </Form.Group>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicCheckbox"
+                      ></Form.Group>
+                      <div className="d-grid">
+                        <Button variant="primary" type="submit">
+                          Ingresar
+                        </Button>
+                      </div>
+                    </Form>
+                    <div className="mt-3">
+                      <p className="mb-0  text-center">
+                        Desea recuperar contrasena?{" "}
+                        <a href="/" className="text-primary fw-bold">
+                          Click aqui
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default LOGIN;
