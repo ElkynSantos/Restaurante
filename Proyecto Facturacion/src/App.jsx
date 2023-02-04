b7eabb6eb35db1b47d6cc9c46628eb248187ad58
EDGAR RICARDO AYALA POLANCO <ericardo_polanco@unitec.edu>
2023-02-04 15:59:33 -0600

    merge

--- Proyecto Facturacion/src/App.jsx
+++ Proyecto Facturacion/src/App.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import ROLES from "./Components/ROLES/index";
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LOGIN from './Components/LOGIN/index';
->>>>>>> main
 
 import INICIO from './Components/INICIO/index';
 
@@ -11,18 +6,7 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-<<<<<<< HEAD
   return <ROLES />;
-=======
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<LOGIN />} />
-                <Route path="/Home" element={<INICIO />} />
-            </Routes>
-        </BrowserRouter>
-    );
->>>>>>> main
 }
 
 export default App;
