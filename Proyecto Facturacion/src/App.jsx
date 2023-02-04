20d2383cb46fa300603a3ce664a9a979d3768dc2
ElkynSantos <elkynmendoza@unitec.edu>
2023-02-03 18:38:55 -0600

    LOGIN NO ESTABA ACTUALIZADO BIEN

--- Proyecto Facturacion/src/App.jsx
+++ Proyecto Facturacion/src/App.jsx
@@ -1,17 +1,20 @@
-import LOGIN from "./Components/LOGIN/index";
-import Barralateral from "./Components/common";
-import INICIO from "./Components/INICIO/index";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LOGIN from './Components/LOGIN/index';
+import Barralateral from './Components/common';
+import INICIO from './Components/INICIO/index';
 
-import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-  return (
-    <div>
-      <Barralateral />
-      <INICIO />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<LOGIN />} />
+                <Route path="/Home" element={<INICIO />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- Proyecto Facturacion/src/Components/LOGIN/Login.jsx
+++ Proyecto Facturacion/src/Components/LOGIN/Login.jsx
@@ -1,102 +1,137 @@
-import React, { useState } from "react";
-import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
-import "./LOGIN.css";
+import React, { useState } from 'react';
+import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap';
+import './LOGIN.css';
 
 function LOGIN() {
-  const [form, setForm] = useState({});
-  const [errors, setErrors] = useState({});
-  const setField = (field, value) => {
-    setForm({
-      ...form,
-      [field]: value,
-    });
-  };
-  function findErrors() {
-    const newErrors = {};
-    let { email, password } = form;
+    const [form, setForm] = useState({});
+    const [errors, setErrors] = useState({});
+    const setField = (field, value) => {
+        setForm({
+            ...form,
+            [field]: value,
+        });
+    };
+    function findErrors() {
+        const newErrors = {};
+        let { email, password } = form;
 
-    if ((!email && email !== "") || email == "") {
-      //En realidad es username
-      newErrors.email = "Espacio de Username Vacio !";
-      //email = "";
+        if ((!email && email !== '') || email == '') {
+            //En realidad es username
+            newErrors.email = 'Espacio de Username Vacio !';
+            //email = "";
+        }
+        // validate with regex
+        if ((!password && password !== '') || password == '') {
+            newErrors.password = 'Espacio de contrasena vacio !';
+            //password = "";
+        }
+
+        console.log(newErrors.password);
+        return newErrors;
     }
-    // validate with regex
-    if ((!password && password !== "") || password == "") {
-      newErrors.password = "Espacio de contrasena vacio !";
-      //password = "";
+    async function handleSubmit(e) {
+        e.preventDefault();
+        let newErrors = findErrors();
+
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+        } else {
+            //LLAMEN A LA API
+        }
     }
 
-    console.log(newErrors.password);
-    return newErrors;
-  }
-  async function handleSubmit(e) {
-    e.preventDefault();
-    let newErrors = findErrors();
+    return (
+        <div>
+            <Container>
+                <Row className="vh-100 d-flex justify-content-center align-items-center">
+                    <Col md={88} lg={100} xs={15}>
+                        <div className="border border-3 border-primary"></div>
+                        <Card className="shadow">
+                            <Card.Body>
+                                <div className="mb-3 mt-md-4">
+                                    <h2 className="fw-bold mb-2 text-uppercase ">
+                                        Iniciar Sesion
+                                    </h2>
+                                    <p>
+                                        Por favor ingrese correo electronico y
+                                        Contrasena
+                                    </p>
+                                    <div className="mb-3">
+                                        <Form onSubmit={handleSubmit}>
+                                            <Form.Group
+                                                className="mb-3"
+                                                controlId="formBasicEmail"
+                                            >
+                                                <Form.Label className="text-center">
+                                                    Direccion de correo
+                                                </Form.Label>
+                                                <Form.Control
+                                                    type="text"
+                                                    placeholder="Ingrese Usuario"
+                                                    onChange={(e) =>
+                                                        setField(
+                                                            'email',
+                                                            e.target.value
+                                                        )
+                                                    }
+                                                    isInvalid={!!errors.email}
+                                                />
+                                            </Form.Group>
 
-    if (Object.keys(newErrors).length > 0) {
-      setErrors(newErrors);
-    } else {
-      //LLAMEN A LA API
-    }
-  }
-
-  return (
-    <div>
-      <Container>
-        <Row className="vh-100 d-flex justify-content-center align-items-center">
-          <Col md={88} lg={6} xs={15}>
-            <div className="border border-3 border-primary"></div>
-            <Card className="shadow">
-              <Card.Body>
-                <div className="mb-3 mt-md-4">
-                  <h2 className="fw-bold mb-2 text-uppercase ">
-                    Iniciar Sesion
-                  </h2>
-                  <div className="mb-3">
-                    <Form onSubmit={handleSubmit}>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label className="text-center">
-                          Nombre de usuario
-                        </Form.Label>
-                        <Form.Control
-                          onChange={(e) => setField("email", e.target.value)}
-                          isInvalid={!!errors.email}
-                        />
-                      </Form.Group>
-
-                      <Form.Group
-                        className="mb-3"
-                        controlId="formBasicPassword"
-                      >
-
-                      </Form.Group>
-                      <Form.Group
-                        className="mb-3"
-                        controlId="formBasicCheckbox"
-                      ></Form.Group>
-                      <div className="d-grid">
-                        <Button variant="primary" type="submit">
-                          Ingresar
-                        </Button>
-                      </div>
-                    </Form>
-                    <div className="mt-3">
-                      <p className="mb-0  text-center">
-                        Desea recuperar contrasena?{" "}
-                        <a href="/" className="text-primary fw-bold">
-                          Click aqui
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+                                            <Form.Group
+                                                className="mb-3"
+                                                controlId="formBasicPassword"
+                                            >
+                                                <Form.Label>
+                                                    Contrasena
+                                                </Form.Label>
+                                                <Form.Control
+                                                    type="password"
+                                                    placeholder="Contrasena"
+                                                    onChange={(e) =>
+                                                        setField(
+                                                            'password',
+                                                            e.target.value
+                                                        )
+                                                    }
+                                                    isInvalid={
+                                                        !!errors.password
+                                                    }
+                                                />
+                                            </Form.Group>
+                                            <Form.Group
+                                                className="mb-3"
+                                                controlId="formBasicCheckbox"
+                                            ></Form.Group>
+                                            <div className="d-grid">
+                                                <Button
+                                                    variant="primary"
+                                                    type="submit"
+                                                >
+                                                    Ingresar
+                                                </Button>
+                                            </div>
+                                        </Form>
+                                        <div className="mt-3">
+                                            <p className="mb-0  text-center">
+                                                Desea recuperar contrasena?{' '}
+                                                <a
+                                                    href="/"
+                                                    className="text-primary fw-bold"
+                                                >
+                                                    Click aqui
+                                                </a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default LOGIN;
