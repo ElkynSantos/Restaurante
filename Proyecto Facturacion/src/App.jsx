74b7772ba05f511b69df987b079295fefa510ead
RicardoVasquez1 <ricardokr@unitec.edu>
2023-02-04 18:47:34 -0600

    Form y Recovery

--- Proyecto Facturacion/src/App.jsx
+++ Proyecto Facturacion/src/App.jsx
@@ -1,10 +1,17 @@
-import LOGIN from "./Components/LOGIN/index";
+//import LOGIN from "./Components/LOGIN/index";
 
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 
+import RecuperarContraseña from "./Components/RecoveryPassword/index";
+import FormPass from "./Components/FormPassword/index";
+import ChangePass from "./Components/ChangePassword/index";
+
 function App() {
-  return <LOGIN />;
+  //return <LOGIN />;
+  //return <RecuperarContraseña />;
+  //return <FormPass/>;
+  //return <ChangePass/>;
 }
 
 export default App;
