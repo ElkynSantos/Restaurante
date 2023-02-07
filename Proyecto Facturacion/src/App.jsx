ed8529254851b7bbf27bb0b5d199312e26321e97
RicardoVasquez1 <ricardokr@unitec.edu>
2023-02-06 23:26:12 -0600

    Actualizando

--- Proyecto Facturacion/src/App.jsx
+++ Proyecto Facturacion/src/App.jsx
@@ -6,12 +6,14 @@ import { useDispatch, useSelector } from "react-redux";
 import RecuperarContraseña from "./Components/RecoveryPassword/index";
 import FormPass from "./Components/FormPassword/index";
 import ChangePass from "./Components/ChangePassword/index";
+import Email from "./Components/EmailVerification/index";
 
 function App() {
   //return <LOGIN />;
   //return <RecuperarContraseña />;
   //return <FormPass/>;
   //return <ChangePass/>;
+ return  <Email/>;
 }
 
 export default App;
