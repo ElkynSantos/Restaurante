41bff4cbbd3ec397cd31de38792ae8121f2eb4eb
Daniel Castro <dpool4fun@unitec.edu>
2023-02-07 23:18:42 -0600

    Añadir Impuesto
    Añadir impuesto a la base de datos

--- server/controllers/taxes.controllers.js
+++ server/controllers/taxes.controllers.js
@@ -19,3 +19,30 @@ const getTaxes = async (req, res, next) => {
         return next(new AppError('Ups! Error en la base de datos', 500));
     }
 }
+
+const addTax = async (req, res, next) => {
+    try{
+        const{
+            descripcion_impuesto,
+            porcentaje_impuesto,
+        } = req.body;
+
+        const emptyParams = Object.values({
+            descripcion_impuesto,
+            porcentaje_impuesto,
+        }).some((val) => !val);
+
+        if (emptyParams){
+            return next(new AppError('Favor completar todos los campos', 401));
+        }
+
+        const [newTax] = await db.query('INSERT INTO db_restaurante.impuestos (descripcion_impuesto, porcentaje_impuesto) VALUES ?, ?', [descripcion_impuesto, porcentaje_impuesto]);
+
+        return res.status(200).json({
+            status: 'Ok',
+        });
+    } catch(error){
+        console.log(error);
+        return next(new AppError('Ups! Error en la base de datos', 500));
+    }
+}
--- server/routes/taxes.routes.js
+++ server/routes/taxes.routes.js
@@ -2,10 +2,11 @@ import  express from 'express';
 
 import {
     getTaxes,
+    addTax,
 } from '../controllers/orders.controllers.js';
 
 const taxesRoutes = express.Router();
 
-taxesRoutes.route('/').get(getTaxes);
+taxesRoutes.route('/').get(getTaxes).post(addTax);
 
 export default taxesRoutes;
