da2a43507e53870c00fdf260055231fc3db9d2b4
ariasortez <juan.arias@unitec.edu>
2023-02-19 18:42:29 -0600

    Testing2

--- server/controllers/users.controllers.js
+++ server/controllers/users.controllers.js
@@ -105,6 +105,7 @@ const createUser = async (req, res, next) => {
                     userEmail: email,
                     userPass: hashedPass,
                     userToken: token,
+                    s,
                 },
             }
         );
