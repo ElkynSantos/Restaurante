e15e3a038bd52c46d60d7f5fde9a5a21dd2b768c
Josue <Josuepinro21@unitec.edu>
2023-02-23 11:42:34 -0600

    Merges

--- Proyecto Facturacion/src/App.jsx
+++ Proyecto Facturacion/src/App.jsx
@@ -19,9 +19,6 @@ import LISTAFACTURACION from './Components/LISTAFACTURACION/index';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 
-import RecuperarContraseña from './Components/RecoveryPassword/index';
-import FormPass from './Components/FormPassword/index';
-import ChangePass from './Components/ChangePassword/index';
 import Email from './Components/EmailVerification/index';
 import IMPUESTO from './Components/IMPUESTOS';
 import ECBMPUESTO from './Components/EDITAR,CREAR,BORRAR IMPUESTOS';
@@ -52,6 +49,7 @@ function App() {
                     path="/ListaFacturacion"
                     element={<LISTAFACTURACION />}
                 />
+                <Route path="/Impuestos" element={<ECBMPUESTO />} />
             </Routes>
         </BrowserRouter>
     );
