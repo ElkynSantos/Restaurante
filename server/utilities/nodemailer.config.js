fea7d634811d3228829358b6dd32cb63cc17774c
ariasortez <juan.arias@unitec.edu>
2023-02-23 23:17:27 -0600

    ProductCode/NodeMailer/LoginCompleted

--- server/utilities/nodemailer.config.js
+++ server/utilities/nodemailer.config.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 const forgotPassordEmail = async (options) => {
     // ? Transporter
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
+        service: process.env.EMAIL_SERVICE,
         port: process.env.EMAIL_PORT,
         auth: {
             user: process.env.USER_EMAIL,
@@ -45,20 +45,19 @@ const forgotPassordEmail = async (options) => {
         to: email,
         subject: 'Cambio de contraseña',
         text: 'Confirma el cambio de tu contraseña',
-        html: `
-
-        <p> ¡Hola ${name}!,</p>
-
-        <p>Se ha solicitado un correo de cambio de contraseña</p>
-
-        <p>Para completar el proceso, por favor haz click en el siguiente enlace: 
-        <a href="http://localhost:5173/auth/reset-password/${token}"> Cambiar Contraseña</a></p>
-        
-        <p>Si no has solicitado un cambio de contraseña, por favor ignora este correo</p>
-
-        <p>Saludos,</p>
-        <p>Garifunas Food</p>
-        `,
+        html: contentHTML,
+        attachments: [
+            {
+                filename: 'logo.png',
+                path: `Proyecto Facturacion/public/assets/images/logo.png`,
+                cid: 'logo',
+            },
+            {
+                filename: 'palm-blue.webp',
+                path: `server/public/assets/palm-blue.png`,
+                cid: 'palm-blue',
+            },
+        ],
     };
 
     // ? 3 Send the email
