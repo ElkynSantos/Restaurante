cb7b5a91f49a1d4a93140c0f75a57611f246cf4c
ariasortez <juan.arias@unitec.edu>
2023-03-01 00:05:00 -0600

    test

--- Proyecto Facturacion/src/App.jsx
+++ Proyecto Facturacion/src/App.jsx
@@ -8,13 +8,13 @@ import MESAS from './Components/MESAS/index';
 import LISTAPEDIDOS from './Components/LISTAPEDIDOS/index';
 import PEDIDOS from './Components/PEDIDOS/index';
 import FACTURACION from './Components/FACTURACION/index';
-import USERS from './Components/USERS/index';
+import USERS from './Components/USERS/index.js';
 import FormPassword from './Components/FormPassword/index';
 import Productos from './Components/CREARPRODUCTOS/index';
 import Recovery from './Components/Recovery/Recovery';
 import editarProductos from './Components/FormPassword/index';
 
-import LISTAFACTURACION from './Components/LISTAFACTURACION/index';
+import LISTAFACTURACION from './Components/LISTAFACTURACION/index.js';
 
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
