cae2b26ba8f1fef23425f3d46b14933731d35e27
Josue <Josuepinro21@unitec.edu>
2023-03-07 10:00:52 -0600

    Impuestos

--- Proyecto Facturacion/src/Components/IMPUESTOSG/ECBIMPUESTO.jsx
+++ Proyecto Facturacion/src/Components/IMPUESTOSG/ECBIMPUESTO.jsx
@@ -1,56 +1,161 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Swal from 'sweetalert2';
 import './Crear.css';
 import BarraLateral from '../common/index.js';
 import { Container } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteTax, editTax } from '../../services/Taxes';
+import axios from 'axios';
+import Modal from 'react-bootstrap/Modal';
 function ECBMPUESTO() {
+    const [show, setShow] = useState(false);
+
     const [nombreImpuesto, setNombreImpuesto] = useState('');
     const [porcentajeImpuesto, setPorcentajeImpuesto] = useState('');
     const [impuestos, setImpuestos] = useState([]);
     const [impuestoEditando, setImpuestoEditando] = useState(null);
 
-    const handleAddImpuesto = (e) => {
-        e.preventDefault();
-        if (!nombreImpuesto || !porcentajeImpuesto) return; // restricción para valores vacíos
-        if (impuestoEditando !== null) {
-            // editando un impuesto existente
-            setImpuestos(
-                impuestos.map((impuesto, index) =>
-                    index === impuestoEditando
-                        ? {
-                              nombre: nombreImpuesto,
-                              porcentaje: porcentajeImpuesto,
-                          }
-                        : impuesto
-                )
-            );
-            setNombreImpuesto('');
-            setPorcentajeImpuesto('');
-            setImpuestoEditando(null);
-        } else {
-            // agregando un nuevo impuesto
-            setImpuestos([
-                ...impuestos,
-                { nombre: nombreImpuesto, porcentaje: porcentajeImpuesto },
-            ]);
-            setNombreImpuesto('');
-            setPorcentajeImpuesto('');
-        }
+    const handleShow = () => setShow(true);
+
+    const handleEditClick = (id) => {
+        handleShow();
+        setImpuestoEditando(id);
+
+        console.log('EDITCLICK');
     };
 
-    const handleEditImpuesto = (index) => {
-        const impuesto = impuestos[index];
-        setNombreImpuesto(impuesto.nombre);
-        setPorcentajeImpuesto(impuesto.porcentaje);
-        setImpuestoEditando(index);
+    const handleClose = () => {
+        setShow(false);
+        setNombreImpuesto('');
+        setPorcentajeImpuesto('');
+        console.log('CLOSE');
     };
 
-    const handleDeleteImpuesto = (index) => {
-        setImpuestos(impuestos.filter((_, i) => i !== index));
+    const handleEditImpuesto = async (taxNameParam, taxAmountParam) => {
+        await fetch('http://localhost:3000/taxes/', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                taxId: impuestoEditando,
+                taxName: taxNameParam,
+                taxAmount: taxAmountParam,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: data.msg,
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+            });
+
+        await fetch('http://localhost:3000/taxes')
+            .then((response) => response.json())
+            .then((DATA) => {
+                console.log(DATA.allTaxes);
+                setImpuestos(DATA.allTaxes);
+            });
+
+        setNombreImpuesto('');
+        setPorcentajeImpuesto('');
+
+        //   const impuesto = impuestos[index];
+        //   setNombreImpuesto(impuesto.nombre);
+        //  setPorcentajeImpuesto(impuesto.porcentaje);
+        //   setImpuestoEditando(index);
     };
 
+    const handleDeleteImpuesto = async (idImpuesto, status) => {
+        console.log('ID: ' + idImpuesto);
+
+        await fetch('http://localhost:3000/taxes/', {
+            method: 'delete',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                taxId: idImpuesto,
+                taxStatus: status ? false : true,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: data.msg,
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+            });
+
+        await fetch('http://localhost:3000/taxes')
+            .then((response) => response.json())
+            .then((DATA) => {
+                console.log(DATA.allTaxes);
+                setImpuestos(DATA.allTaxes);
+            });
+
+        //  setImpuestos(impuestos.filter((_, i) => i !== index));
+    };
+    useEffect(() => {
+        const getAllTaxes = async () => {
+            await fetch('http://localhost:3000/taxes')
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data.allTaxes);
+                    setImpuestos(data.allTaxes);
+                });
+        };
+
+        getAllTaxes();
+    }, [nombreImpuesto, porcentajeImpuesto]);
+
+    const handleAddImpuesto = async (taxNameParam, taxAmountParam) => {
+        await fetch('http://localhost:3000/taxes/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                taxDescription: taxNameParam,
+                taxAmount: taxAmountParam,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: data.msg,
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+            });
+        await fetch('http://localhost:3000/taxes')
+            .then((response) => response.json())
+            .then((DATA) => {
+                console.log(DATA.allTaxes);
+                setImpuestos(DATA.allTaxes);
+            });
+
+        setNombreImpuesto('');
+        setPorcentajeImpuesto('');
+    };
     return (
         <Container>
             <BarraLateral />
@@ -67,33 +172,121 @@ function ECBMPUESTO() {
                     </thead>
                     <tbody>
                         {impuestos.map((impuesto, index) => (
-                            <tr key={index}>
-                                <td>{impuesto.nombre}</td>
-                                <td>{impuesto.porcentaje}</td>
+                            <tr key={impuesto.id}>
+                                <td>{impuesto.name}</td>
+                                <td>{impuesto.amount}</td>
                                 <td>
-                                    <Button
-                                        variant="warning"
-                                        className="mr-2"
-                                        onClick={() =>
-                                            handleEditImpuesto(index)
-                                        }
-                                    >
-                                        Editar
-                                    </Button>
-                                    <Button
-                                        variant="danger"
-                                        onClick={() =>
-                                            handleDeleteImpuesto(index)
-                                        }
-                                    >
-                                        Eliminar
-                                    </Button>
+                                    <>
+                                        <Button
+                                            variant="warning"
+                                            className="mr-2"
+                                            onClick={() =>
+                                                handleEditClick(impuesto.id)
+                                            }
+                                        >
+                                            Editar
+                                        </Button>
+
+                                        <Modal show={show} onHide={handleClose}>
+                                            <Modal.Header closeButton>
+                                                <Modal.Title>
+                                                    Editar Impuesto
+                                                </Modal.Title>
+                                            </Modal.Header>
+                                            <Modal.Body>
+                                                <Form>
+                                                    <Form.Group>
+                                                        <Form.Label>
+                                                            Nombre del impuesto
+                                                        </Form.Label>
+                                                        <Form.Control
+                                                            type="text"
+                                                            value={
+                                                                nombreImpuesto
+                                                            }
+                                                            onChange={(e) =>
+                                                                setNombreImpuesto(
+                                                                    e.target
+                                                                        .value
+                                                                )
+                                                            }
+                                                        />
+                                                    </Form.Group>
+                                                    <Form.Group>
+                                                        <Form.Label>
+                                                            Porcentaje del
+                                                            impuesto
+                                                        </Form.Label>
+                                                        <Form.Control
+                                                            type="text"
+                                                            value={
+                                                                porcentajeImpuesto
+                                                            }
+                                                            onChange={(e) =>
+                                                                setPorcentajeImpuesto(
+                                                                    e.target
+                                                                        .value
+                                                                )
+                                                            }
+                                                        />
+                                                    </Form.Group>
+                                                </Form>
+                                            </Modal.Body>
+                                            <Modal.Footer>
+                                                <Button
+                                                    variant="danger"
+                                                    onClick={handleClose}
+                                                >
+                                                    Close
+                                                </Button>
+                                                <Button
+                                                    variant="warning"
+                                                    className="mr-2"
+                                                    onClick={() => {
+                                                        handleEditImpuesto(
+                                                            nombreImpuesto,
+                                                            porcentajeImpuesto
+                                                        );
+                                                        handleClose();
+                                                    }}
+                                                >
+                                                    Guardar Cambios
+                                                </Button>
+                                            </Modal.Footer>
+                                        </Modal>
+                                    </>
+
+                                    {impuesto.status ? (
+                                        <Button
+                                            variant="success"
+                                            onClick={() =>
+                                                handleDeleteImpuesto(
+                                                    impuesto.id,
+                                                    impuesto.status
+                                                )
+                                            }
+                                        >
+                                            Habilitado
+                                        </Button>
+                                    ) : (
+                                        <Button
+                                            variant="danger"
+                                            onClick={() =>
+                                                handleDeleteImpuesto(
+                                                    impuesto.id,
+                                                    impuesto.status
+                                                )
+                                            }
+                                        >
+                                            Deshabilitado
+                                        </Button>
+                                    )}
                                 </td>
                             </tr>
                         ))}
                     </tbody>
                 </Table>
-                <Form onSubmit={handleAddImpuesto}>
+                <Form>
                     <Form.Group>
                         <Form.Label>Nombre del impuesto</Form.Label>
                         <Form.Control
@@ -112,7 +305,16 @@ function ECBMPUESTO() {
                             }
                         />
                     </Form.Group>
-                    <Button variant="primary" type="submit" className="mt-3">
+                    <Button
+                        variant="primary"
+                        className="mt-3"
+                        onClick={() =>
+                            handleAddImpuesto(
+                                nombreImpuesto,
+                                porcentajeImpuesto
+                            )
+                        }
+                    >
                         Agregar Impuesto
                     </Button>{' '}
                     <Button
@@ -133,3 +335,19 @@ function ECBMPUESTO() {
 }
 
 export default ECBMPUESTO;
+
+/*
+                                    <Button
+                                        variant="warning"
+                                        className="mr-2"
+                                        onClick={() =>
+                                            handleEditImpuesto(
+                                                impuesto.id,
+                                                nombreImpuesto,
+                                                porcentajeImpuesto
+                                            )
+                                        }
+                                    >
+                                        Editar
+                                    </Button>
+*/
